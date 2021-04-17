4e7e352456360631a38a063bd95507c56ac0243e
adriaalcala <adria.alcala@gmail.com>
2021-04-17 10:56:27 +0200

    Feature/add info (#1)
    * Implement info alignments tab (WIP)

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import dash_table
 
 from flask import request, Flask
 
-from dashboard.pinaweb_results.callback import update_graph, update_table, layout
-from texts import GRAPH_DESCRIPTION
+from dashboard.pinaweb_results.callback import update_graph, update_table, layout, get_info_data
+from texts import GRAPH_DESCRIPTION, TABLE_DESCRIPTION
 # ======== #
 # DASH APP #
 # ======== #
@@ -27,42 +27,53 @@ app = dash.Dash(
 app.layout =html.Div(
     [
         dcc.Location(id='url', refresh=False), html.H1('PINAWEB RESULTS', id='H1-text', style={'textAlign': 'center'}),
-        html.P(GRAPH_DESCRIPTION, style={'margin': '60px'}), html.Div(id='page-content', children=
-        [html.Div(id='concensus-plot'),
-          html.Div([
-                html.P('Page size: ', id='page-size-text', style={'margin-left': '60px'}),
-                dcc.Input(id='table-concensus-plot-page-count', type='number', min=1, max=100000, value=20, style={'margin': '6px'})
-          ], style={'display': 'flex'}),
-            html.Div(id='div-dash-table', children=dash_table.DataTable(
-                id='table-concensus-plot',
-                columns=[],
-                page_current=0,
-                page_size=20,
-                page_action='custom',
-                filter_action='custom',
-                filter_query='',
-                sort_action='custom',
-                sort_mode='multi',
-                sort_by=[],
-                style_table={'overflowY': 'auto', 'overflowX': 'auto', 'width': '80%', 'margin_left': '10px'},
-                style_cell={
-                    'height': 'auto',
-                    # all three widths are needed
-                    'minWidth': '10px', 'width': '10px', 'maxWidth': '10px',
-                    'whiteSpace': 'normal'
-                }
+        dcc.Tabs([
+            dcc.Tab(label='Comparison', children=[
+                dcc.Markdown(GRAPH_DESCRIPTION, style={'margin': '60px'}),
+                html.Div(id='page-content', children=[  
+                    html.Div(id='concensus-plot'),
+                html.P(TABLE_DESCRIPTION, style={'margin': '60px'}),
+                    html.Div([
+                        html.P('Page size: ', id='page-size-text', style={'margin-left': '60px'}),  
+                        dcc.Input(id='table-concensus-plot-page-count', type='number', min=1, max=100000, value=20, style={'margin': '6px'})
+                    ],
+                    style={'display': 'flex'}
+                    ),
+                    html.Div(id='div-dash-table', children=dash_table.DataTable(
+                            id='table-concensus-plot',
+                            columns=[],
+                            page_current=0,
+                            page_size=20,
+                            page_action='custom',
+                            filter_action='custom',
+                            filter_query='',
+                            sort_action='custom',
+                            sort_mode='multi',
+                            sort_by=[],
+                            style_table={'overflowY': 'auto', 'overflowX': 'auto', 'width': '80%', 'margin_left': '10px'},
+                            style_cell={
+                                'height': 'auto',
+                                # all three widths are needed
+                                'minWidth': '10px', 'width': '10px', 'maxWidth': '10px',
+                                'whiteSpace': 'normal'
+                            }
 
-            )
-            ),
-    ])
-]
+                        )
+                    ),
+                ])
+            ]),
+            dcc.Tab(label='Alignments info', id="info"),
+
+        ])
+    ]
 )
 
 
 @app.callback(
     [Output('table-concensus-plot', "data"),
     Output('table-concensus-plot', "columns"),
-    Output('concensus-plot', "children")],
+    Output('concensus-plot', "children"),
+    Output('info', "children")],
     [Input('table-concensus-plot', "page_current"),
     Input('table-concensus-plot', "page_size"),
     Input('table-concensus-plot', "sort_by"),
@@ -70,7 +81,8 @@ app.layout =html.Div(
 def update_consensus_table(page_current, page_size, sort_by, filter_query):
     job_id = request.headers['Referer'].split('/')[-1]
     rows, columns = update_table(job_id, page_current, page_size, sort_by, filter_query)
-    return rows, columns, update_graph(rows)
+    info_data = get_info_data(job_id)
+    return rows, columns, update_graph(rows), info_data
 
 @app.callback(
     Output('table-concensus-plot', "page_size"),
@@ -79,12 +91,6 @@ def update_page_count(page_count_value):
     if page_count_value is None:
         return 20
     return page_count_value
-
-@app.callback([Output("protein-images", "children")],
-              [Input("dropdown-proteines", "value")],
-              )
-def get_protein_images(protein):
-    return None
 
 
 # ======= #
@@ -95,4 +101,4 @@ app.config['suppress_callback_exceptions'] = True
 
 # Run Dash on a Public IP
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=int("8080"), debug=False, dev_tools_hot_reload = False, use_reloader=True)
+    app.run_server(host="0.0.0.0", port=int("5000"), debug=False, dev_tools_hot_reload = False, use_reloader=True)
--- texts.py
+++ texts.py
@@ -1,11 +1,29 @@
 GRAPH_DESCRIPTION = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut diam facilisis, finibus erat iaculis, facilisis mi. Ut cursus eu ipsum vel malesuada. Proin enim urna, gravida vel accumsan quis, condimentum eu nibh. Cras sit amet dictum felis, pharetra ullamcorper nunc. Donec sit amet malesuada dolor. Etiam arcu justo, feugiat quis eleifend nec, accumsan quis orci. Fusce ac molestie ante. Suspendisse aliquam ultricies condimentum. Proin euismod eros vitae felis commodo pharetra. Donec finibus, lectus dignissim porta fermentum, tortor urna laoreet urna, ut interdum elit arcu a nisl.
+The heatmap below  displays  the comparison of the alignments results.
+Every selected aligner is displayed on the left in the heatmap and all
+proteins from the input network are listed as columns. The agreement
+level between the aligned proteins is represented through a color
+palette that ranges from light green to dark green:
+*  Dark green indicates a total agreement, meaning that all aligners have matched the protein to
+the same protein in the output network.
+* Light green color means that all aligners matched the corresponding protein to a different one in the
+output network. Thus, a vertical column in dark green shows a consensus
+among all aligners.
+* The color corresponding to unaligned proteins is
+gray.
 
-Sed congue nulla id ligula fringilla hendrerit. Pellentesque viverra augue lectus, ac mollis leo tincidunt sed. Donec erat libero, sollicitudin id eros vitae, euismod dictum urna. Proin convallis nibh sed aliquam finibus. Aenean ut sem vitae justo cursus tincidunt. Curabitur nec ante eu nibh finibus viverra ut nec arcu. Ut suscipit eget ex a cursus. Fusce dictum quam ligula, sed molestie eros feugiat nec. Sed vestibulum volutpat dolor eu euismod.
+In addition, when pointing the cursor in the columns on the
+heatmap a window with the following information is shown: 
+* *Origin*: the protein name in the input network.
+* *Target*: the protein name in the output network to whom the origin protein has been assigned by the aligner.
+* *Aligner*: the corresponding aligner.
+"""
 
-Fusce lobortis neque nisl, ac fermentum tortor convallis a. Mauris mattis ullamcorper mi et pretium. Donec interdum maximus urna a tempus. Donec varius metus ac libero porta, egestas fringilla mi ornare. Duis vel convallis dolor. Fusce velit elit, condimentum quis erat in, porta bibendum mi. Sed bibendum convallis tellus, non porta tellus commodo sit amet. Nunc vitae eros ipsum. Aenean eget sapien non ipsum tincidunt hendrerit. Praesent nec massa est.
-
-Donec fringilla nibh vel massa egestas consectetur. Phasellus sodales magna porttitor dui tristique sagittis eget ut enim. Ut fringilla odio turpis, nec accumsan eros pulvinar sit amet. Proin mollis at velit a tristique. Suspendisse tortor erat, egestas et nunc et, consequat gravida justo. Sed laoreet tortor eros, a pharetra erat luctus et. Donec id turpis felis. Suspendisse auctor justo at diam dapibus, et posuere magna dictum. Nullam hendrerit ut turpis sit amet iaculis.
-
-Vestibulum at lacus sit amet mi tempus euismod. Curabitur vel sollicitudin neque, sed mattis elit. Etiam venenatis feugiat sem, nec auctor urna tempus vitae. Nulla sit amet viverra tortor. Sed arcu sem, accumsan sed egestas at, ultrices non velit. Suspendisse posuere nunc vitae sem laoreet sollicitudin. Quisque non nibh vitae.
+TABLE_DESCRIPTION = """
+In the page size below the number of proteins (columns) to visualize in
+the heatmap can be selected.  Also, the table displays the alignment
+results. For every row, the first column is the protein name in the
+input network. Its corresponding matching for every aligner are listed
+in the other columns. Last column provides a consensus score which is
+the mean of agreement among all aligners.
 """
